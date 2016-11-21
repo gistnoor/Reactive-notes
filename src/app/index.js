4bdaa0729d9f8d8376a6d813ea39523bab9bc32c
Noor <gist.asifhasannoor@gmail.com>
2016-11-22 05:21:37 +0600

    Some modification on notepad

---
+++ src/app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Notes from './components/Notes';
+
+
+require('../views/styles/style.css');
+ReactDOM.render(<Notes />, document.getElementById('notes'));
